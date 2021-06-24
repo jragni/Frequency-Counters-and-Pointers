06cd7843a05a1b8c08ead9e2108216d6e93b4532
jragni <jagni001@ucr.edu>
2021-06-24 00:07:05 -0700

    FEAT: completed and passed isSubsequence.js jasmine tests

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,29 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+"use strict"
+/**
+ * isSubsequenct takes in two strings and checks whether the characters in the first string form a subsequence of characters
+ * in the second string.
+ * @param {String} subString  --- subString the function verifies exists within string
+ * @param {String} string  --- string the function checks to see if subString exists 
+ * @returns {Boolean} 
+ */
+function isSubsequence(subString, string) {
+
+	// implement multiple pointers
+
+	let subStringIdx = 0;
+	let stringIdx = 0;
+	while(stringIdx < string.length){
+
+		if(string[stringIdx] === subString[subStringIdx]){
+			subStringIdx++;
+		}
+
+		stringIdx++;
+
+		if(subStringIdx === subString.length){
+			return true;
+		}
+	}
+
+	return false;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,29 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+"use strict"
+
+/**
+ * 
+ * @param {Array} array -- unsorted array of integers 
+ * @returns sorted array of integers separated with positive at the beggining and negative at the end
+ */
+
+function separatePositive(array) {
+
+	let leftPtr = 0;
+	let rightPtr = array.length - 1;
+
+	// Use multiple pointers pattern to swap
+	while(leftPtr < rightPtr){
+		// if left element is negative and right element is positive, swap and increment left/decrement right
+		if(array[leftPtr] < 0 && array[rightPtr] > 0){
+			[ array[leftPtr], array[rightPtr] ] = [ array[rightPtr], array[leftPtr] ];
+			leftPtr++;
+			rightPtr--;
+		}else if (array[leftPtr] > 0){
+			leftPtr++;
+		}else{
+			rightPtr--; 
+		}
+	}
+
+	return array;
 }
