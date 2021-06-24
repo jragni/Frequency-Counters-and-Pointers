85eac6385bfbf766704a6198f88b6cb086e14e07
jragni <jagni001@ucr.edu>
2021-06-24 00:21:07 -0700

    completed count-pairs.js and passed all jasmine tests

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,16 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+"use strict"
+
+function countPairs(array, target) {
+
+	let count = 0;
+	let potentialPairs = new Set(array);
+	
+	for(let value of potentialPairs){
+		
+		potentialPairs.delete(value);
+		if(potentialPairs.has(target - value)){
+			count++;
+		}
+	}
+	return count;
 }
