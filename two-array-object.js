fb005380a408a6d768e90ee7cc4f4660b85ebb2a
jragni <jagni001@ucr.edu>
2021-06-23 23:19:51 -0700

    FEAT: completed and passed twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,21 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+"use strict"
+/**
+ * 
+ * @param {Array} keys  -- keys for the object
+ * @param {Array} values  -- values for the object
+ * @returns {Object} -- object made from keys and values
+ */
+function twoArrayObject(keys, values) {
+
+	let keyValueObject = {};
+
+	keys.forEach( (key, idx) => {
+		
+		let value = values[idx] !== undefined ? values[idx] : null; 
+
+		keyValueObject[key] = value;
+	})
+
+	return keyValueObject;
 }
+
