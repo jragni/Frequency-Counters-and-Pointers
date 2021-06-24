405086a4f2014d0752e76a3e358cc593340dd56c
jragni <jagni001@ucr.edu>
2021-06-24 00:53:05 -0700

    TODO: needs to complete pivot-index.js

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,9 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+"use strict"
+/**
+ * pivotIndex accepts an array of integers and returns the pivot index where the sum of the items to the left equal the
+ * sum of the items to the right
+ * @param {Array} array -- array of integers
+ * @returns {Integer}  index of where the sum of the items to the left equal the sum of the items to the right
+ */
+function pivotIndex(array) {
 }
