931b0540ded3dcd56e1215c11e945c71d0cab5cd
jragni <jagni001@ucr.edu>
2021-06-23 23:03:01 -0700

    Feat: completed and passed all jasmine tests for can-construct-word.js

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict"
+
+function canConstructWord(word, letters) {
+	
+	// Edge cases
+	if(word.length > letters.length){
+		return false;
+	}
+
+	let wordCharCounts = frequencyCounter(word);
+	let letterCounts = frequencyCounter(letters);
+
+	for(let key in wordCharCounts){
+		if( letterCounts[key] < wordCharCounts[key]){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+function frequencyCounter(string){
+	let charCounts = {}
+	
+	for(let char of string){
+		charCounts[char] ? charCounts[char]++ : charCounts[char] = 1;
+	}
+
+	return charCounts;
 }
