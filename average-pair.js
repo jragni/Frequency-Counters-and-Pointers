e041228e25f3e50020d027a85f63b1db370a9256
jragni <jagni001@ucr.edu>
2021-06-23 23:12:11 -0700

    FEAT: completed average-pair.js and passed all the jasmine tests" " ;

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,32 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+"use strict"
+
+/**
+ * 
+ * @param {Integer} array -- Sorted array of integers 
+ * @param {Integer} target -- target average that the function is trying to search the array of integers for
+ * @returns {boolean}  -- boolean of whether the pair that averages the target exists
+ */
+
+function averagePair(array, target) {
+	let leftPtr = 0;
+	let rightPtr = array.length - 1;
+
+	// Implement Multiple Pointers because the input array is sorted
+	while (leftPtr < rightPtr){
+		let currentAverage = calculatePairAverage(array[leftPtr], array[rightPtr]);
+
+		if (currentAverage === target){
+			return true;
+		}else if (currentAverage < target ){
+			leftPtr++;
+		}else{
+			rightPtr --;
+		}
+	}
+	return false;
+
+}
+
+function calculatePairAverage (num1, num2){
+	return (num1 + num2) / 2;
 }
