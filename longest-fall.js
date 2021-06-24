76e426c1cb2868bf4caa03c8a0c8df51e9fabda4
jragni <jagni001@ucr.edu>
2021-06-24 00:46:19 -0700

    FEAT: completed longest-fall.js

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(array) {
+	// Edge cases
+	if(array.length === 1 ){ 
+		return 1
+	}else if(array.length === 0) {
+		return 0;
+	}
+	
+
+	let longestConsecutiveFall = 1;
+	let idx = 0;
+	let currentCounter = 1;
+
+	while(idx < array.length ){
+
+		if(currentCounter > longestConsecutiveFall){
+			longestConsecutiveFall = currentCounter;
+		}
+		array[idx] > array[idx + 1] ? currentCounter++ : currentCounter = 1;
+
+		idx++;
+	}
+	return longestConsecutiveFall;
 }
